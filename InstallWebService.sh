519239b39e0a59226af8ad210e669d96534056db
david <david.levin.1986@gmail.com>
2025-04-14 18:58:10 +0300

    test script

--- InstallWebService.sh
+++ InstallWebService.sh
@@ -4,11 +4,13 @@
 if [ -f /etc/redhat-release ]; then
     echo "CentOS / RHEL detected"
     SERVICE_NAME="httpd"
-    INSTALL_CMD="sudo yum install -y httpd"
+    INSTALL_WEB="sudo yum install -y httpd"
 elif [ -f /etc/debian_version ]; then
     echo "Ubuntu / Debian detected"
     SERVICE_NAME="apache2"
-    INSTALL_CMD="sudo apt update && sudo apt install -y apache2"
+    INSTALL_UDATE="sudo apt update -y"
+    INSTALL_WEB="sudo apt install -y apache2"
+    INSTALL_UDATE
 else
     echo "Unsupported OS"
     exit 1
@@ -49,7 +51,7 @@ if sudo systemctl status $SERVICE_NAME &>/dev/null; then
 else
     # install web service if nesesery
     echo "$SERVICE_NAME is NOT installed. Installing..."
-    $INSTALL_CMD
+    INSTALL_WEB
     if [ $? -eq 0 ]; then
         echo "$SERVICE_NAME installed successfully"
 
